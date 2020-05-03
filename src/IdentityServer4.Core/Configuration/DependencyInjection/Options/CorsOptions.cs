15337f3483f1befa05187db26f5b454691345a13
Eivind Gussiås Løkseth <eloekset@msn.com>
2020-05-03 23:03:17 +0200

    Refactor without using git mv to keep file history

---
+++ src/IdentityServer4.Core/Configuration/DependencyInjection/Options/CorsOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+
+namespace IdentityServer4.Configuration
+{
+    /// <summary>
+    /// Options for CORS
+    /// </summary>
+    public class CorsOptions
+    {
+        /// <summary>
+        /// Gets or sets the name of the cors policy.
+        /// </summary>
+        /// <value>
+        /// The name of the cors policy.
+        /// </value>
+        public string CorsPolicyName { get; set; } = Constants.IdentityServerName;
+
+        /// <summary>
+        /// The value to be used in the preflight `Access-Control-Max-Age` response header.
+        /// </summary>
+        public TimeSpan? PreflightCacheDuration { get; set; }
+
+    }
+}
